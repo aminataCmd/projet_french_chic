4d01a15952f9d2240c7d6cada7db7c0e0d3c49f5
Aminata <ami95190@gmail.com>
2021-10-05 19:44:27 +0200

    code du sprint 1

---
+++ sprint_1/french_Chic/src/controlleur/Session.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlleur;
+
+/**
+ *
+ * @author Aminata
+ */
+public class Session {
+    
+    // Attributs
+    
+    
+    //Méthodes
+    public String traiterConnexion(){
+        return "ECRAN_ACCUEIL";
+    }
+}
